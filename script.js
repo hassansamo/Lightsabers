515cc7259cc8088f35774e51101701cc4ecf993c
Hassan Samo <hassansamo609@gmail.com>
2022-08-08 22:45:21 +0500

    added darksaber sound

--- script.js
+++ script.js
@@ -1,19 +1,35 @@
 function playSound() {
-  let lightAudio = document.getElementById("soundEffectLight");
-  let darkAudio = document.getElementById("soundEffectDark");
+  // lightsabers
+  let lsStart = document.getElementById("lsStart");
+  let lsPulse = document.getElementById("lsPulse");
+  let lsStop = document.getElementById("lsStop");
+
+  // darksaber
+  let dsStart = document.getElementById("dsStart");
+  let dsPulse = document.getElementById("dsPulse");
+  let dsStop = document.getElementById("dsStop");
+
   let darkSaber = document.getElementById("darksaber");
+
   let box = document.getElementById("on-off");
-  if (box.checked == true && darkSaber.checked) {
-    darkAudio.play();
+
+  // Sound effects of lightsabers
+  if (box.checked == true && !darkSaber.checked) {
+    lsStart.play();
+    lsPulse.play();
   } else {
-    darkAudio.pause();
-    darkAudio.currentTime = 0;
+    lsPulse.pause();
+    lsPulse.currentTime = 0;
+    lsStop.play();
   }
 
-  if (box.checked == true && !darkSaber.checked) {
-    lightAudio.play();
+  // Sound effects of darksabers
+  if (box.checked == true && darkSaber.checked) {
+    dsStart.play();
+    dsPulse.play();
   } else {
-    lightAudio.pause();
-    lightAudio.currentTime = 0;
+    dsPulse.pause();
+    dsPulse.currentTime = 0;
+    dsStop.play();
   }
 }
